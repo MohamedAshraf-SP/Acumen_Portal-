90de857467cae306650d11d2c898727a268b9653
Mohamed_Al-Mahlawi <74742800+MohamedAshraf-SP@users.noreply.github.com>
2026-01-08 15:38:29 +0300

    Refactor due dates schema to use object type

--- server/models/company/dueDates.js
+++ server/models/company/dueDates.js
@@ -10,13 +10,34 @@ const dueDateSchema = new mongoose.Schema({
     vatNumber: String,
     VATRegistered:Boolean,
     vatReturnsPeriod: String, // annual, quarterly
-    quarter1DueBy: Date|null,
-    quarter2DueBy: Date|null,
-    quarter3DueBy: Date|null,
-    quarter4DueBy: Date|null,
-    confirmationStatementDueBy: Date|null,
-    AccountsDueBy: Date|null,
-    annualVatDueBy: Date|null,
+    quarter1DueBy:{
+  type: Date,
+  default: null
+},
+    quarter2DueBy: {
+  type: Date,
+  default: null
+},
+    quarter3DueBy:{
+  type: Date,
+  default: null
+},
+    quarter4DueBy: {
+  type: Date,
+  default: null
+},
+    confirmationStatementDueBy:{
+  type: Date,
+  default: null
+},
+    AccountsDueBy:{
+  type: Date,
+  default: null
+},
+    annualVatDueBy: {
+  type: Date,
+  default: null
+},
    
 }, { timestamps: true })
 export default mongoose.model('DueDate', dueDateSchema) 
